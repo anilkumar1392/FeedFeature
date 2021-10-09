bbb60712d08cf66fa0c7bf8b9138edc7045ebcb0
Anil kumar <anil1.kumar@africa.airtel.com>
2021-10-09 10:51:44 +0530

    Initial Commit

---
+++ FeedFeature/FeedFeature.h
@@ -0,0 +1,17 @@
+//
+//  FeedFeature.h
+//  FeedFeature
+//
+//  Created by 13401027 on 09/10/21.
+//
+
+
+//! Project version number for FeedFeature.
+FOUNDATION_EXPORT double FeedFeatureVersionNumber;
+
+//! Project version string for FeedFeature.
+FOUNDATION_EXPORT const unsigned char FeedFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedFeature/PublicHeader.h>
+
+
